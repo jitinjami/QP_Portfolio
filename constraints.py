549fe2a5909168295a866d6493669cb2dcb7d0b7
ve38teco <valentin.bacher@fau.de>
2021-11-21 12:22:20 +0100

    updated comments in constraints.py

--- constraints.py
+++ constraints.py
@@ -6,3 +6,15 @@ This file defines constrains
 import numpy as np
 
 # Define Constrains
+
+# equality constrains
+"""
+all weights have to add up to one. One equality constraint
+transaction cost 1: the old value is the new value minus buys and plus sells
+"""
+
+# inequality constrains
+"""
+short sales: all weights have to be positive. Results in n inequality constraints
+transaction cost 2: buys and sells have to be positive but are bound from above. Results in 2n inequality constraints. 
+"""
